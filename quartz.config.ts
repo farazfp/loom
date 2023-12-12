bcad34f98511ead243f1813c72233fa65857bca9
farazfp <faraz.fp@gmail.com>
2023-12-11 21:34:14 -0800

    Quartz sync: Dec 11, 2023, 9:34 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -20,7 +20,7 @@ const config: QuartzConfig = {
       },
       colors: {
         lightMode: {
-          light: "#EDEDE9",
+          light: "#DEDEDE",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
